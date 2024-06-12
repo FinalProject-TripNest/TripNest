6583428a7e0b5ec42a94faf946060b9cc15e059b
Son <ssoo9271@naver.com>
2024-06-12 17:54:59 +0900

    20240612/17:54

---
+++ src/main/java/data/controller/JournalController.java
@@ -0,0 +1,13 @@
+package data.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JournalController {
+
+	@GetMapping("journal")
+	public String index() {
+		return "journal/journal";
+	}
+}
