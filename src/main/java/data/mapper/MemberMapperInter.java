626d3a10113cd0ea3f0d0972b796401edfa0526b
STAR-mingyu <kmm1604@naver.com>
2024-06-21 15:21:47 +0900

    관리자 회원 리스트

--- src/main/java/data/mapper/MemberMapperInter.java
+++ src/main/java/data/mapper/MemberMapperInter.java
@@ -1,12 +1,20 @@
 package data.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import data.dto.MemberDto;
 
 @Mapper
 public interface MemberMapperInter {
-    void insertMember(MemberDto dto);
-    MemberDto findByEmail(@Param("email") String email);
-    MemberDto findBySocialIdAndType(@Param("social_id") String social_id, @Param("social_type") String social_type);
+	void insertMember(MemberDto dto);
+
+	MemberDto findByEmail(@Param("email") String email);
+
+	MemberDto findBySocialIdAndType(@Param("social_id") String social_id, @Param("social_type") String social_type);
+
+	public List<MemberDto> dataList();
+
+	public void dataDelete(String member_id);
 }
--- src/main/java/data/service/MemberServiceInter.java
+++ src/main/java/data/service/MemberServiceInter.java
@@ -1,9 +1,13 @@
 package data.service;
 
+import java.util.List;
+
 import data.dto.MemberDto;
 
 public interface MemberServiceInter {
     void registerMember(MemberDto dto, String token);
     MemberDto findByEmail(String email);
     boolean checkEmailExists(String email);
+    public List<MemberDto> dataList();
+    public void dataDelete(String member_id);
 }
