2ebbd61916f200dfc8714887a55d670db24ed019
STAR-mingyu <kmm1604@naver.com>
2024-06-26 14:54:44 +0900

    관리자 리뷰 완료

--- src/main/java/data/dto/ReviewJoinDto.java
+++ src/main/java/data/dto/ReviewJoinDto.java
@@ -9,12 +9,13 @@ import lombok.Data;
 @Data
 @Alias("ReviewJoinDto")
 public class ReviewJoinDto {
-
-	private int review_id;
-	private int member_id;
-	private int room_id;
-	private String review_content;
-	private int review_rating;
-	private Timestamp review_date;
-	private String member_name;
+    private int review_id;
+    private int member_id;
+    private int room_id;
+    private String review_content;
+    private int review_rating;
+    private Timestamp review_date;
+    private String member_name;
+    private String member_useremail;
+    private String room_name;
 }
--- src/main/java/data/service/ReviewServiceInter.java
+++ src/main/java/data/service/ReviewServiceInter.java
@@ -2,9 +2,13 @@ package data.service;
 
 import java.util.List;
 
-import data.dto.ReviewDto;
+
 import data.dto.ReviewJoinDto;
 
 public interface ReviewServiceInter {
 	public List<ReviewJoinDto> dataList(String room_id);
+
+    public List<ReviewJoinDto> adminReview();
+    
+    public void dataDelete(String review_id);
 }
